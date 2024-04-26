eb79672a5252e7f9a0b6d38749f3db585d1e6509
Leandro Calcabrini <leacalca@gmail.com>
2024-04-26 13:04:19 -0300

    funciones de click en mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
-const btn = document.querySelectorAll(".btn");
+const btn = document.querySelectorAll(".btn"); 
+const profile = document.querySelector(".profile");
+const containerProfile = document.querySelector(".container-profile");
+const ancho = window.matchMedia('(max-width:450px)');
+console.log(ancho.matches);
 
-btn.forEach((e)=>{
-    e.addEventListener("click",()=>{
-        const share = document.querySelectorAll(".share")
-        share.forEach((i)=>{
-            i.classList.toggle("active")
-        })   
-    })
-})
+
+
+
+
+
+
+
+function click(){
+    if(ancho.matches === true){
+        btn.forEach((e)=>{
+            e.addEventListener("click",()=>{
+                profile.style.display = "none";
+                containerProfile.classList.add("mobile")
+            })
+    
+        })
+       
+    }else{
+        btn.forEach((e)=>{
+            e.addEventListener("click",()=>{
+                const share = document.querySelectorAll(".share")
+                share.forEach((i)=>{
+                    i.classList.toggle("active")
+                })   
+            })
+        })
+    }
+}
+
+
+click();
+
